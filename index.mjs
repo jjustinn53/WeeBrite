6db00ecb3da63c669c0422be0f90139df5477cd6
deathtaco1408 <61480526+deathtaco1408@users.noreply.github.com>
2025-11-20 16:48:07 -0800

    implemented routes /character, /
    implemented /character as search function to test functionality

--- index.mjs
+++ index.mjs
@@ -7,13 +7,21 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 // root route
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
    console.log(animeQuotes.randomQuoteByCharacter("Luffy"));
+   res.render('home.ejs');
 });
 
 
 
 app.get('/character', async (req, res) => {
+   const characterName = req.query.name;
+   try {
+      const quoteData = await animeQuotes.randomQuoteByCharacter(characterName);
+      res.render('character.ejs', { quote: quoteData });
+   } catch (error) {
+      res.render('character.ejs', { quote: null, error: 'Character not found or no quotes available.' });
+   }
 });
 
 app.listen(3000, () => {
